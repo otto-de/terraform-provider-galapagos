a57b37e04b92b853126fa865b11c5310f4064edb
Andreas Bergmeier <abergmeier@gmx.net>
2022-06-17 06:01:51 +0200

    Create stubs around Account
    Use google_service_account as a template and go from there.

---
+++ internal/resources/account_type.go
@@ -0,0 +1,42 @@
+package resources
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var (
+	AccountType = &accountType{}
+)
+
+type accountType struct {
+}
+
+func (t *accountType) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
+	return tfsdk.Schema{
+		MarkdownDescription: "FIXME",
+		Attributes: map[string]tfsdk.Attribute{
+			"name": {
+				Type:        types.StringType,
+				Description: "Name of Account",
+				Required:    true,
+				PlanModifiers: tfsdk.AttributePlanModifiers{
+					// TODO: Implement Update
+					tfsdk.RequiresReplace(),
+				},
+			},
+			"id": {
+				Type:        types.StringType,
+				Description: "FIXME",
+				Computed:    true,
+			},
+		},
+	}, nil
+}
+
+func (t *accountType) NewResource(ctx context.Context, p tfsdk.Provider) (tfsdk.Resource, diag.Diagnostics) {
+	return &accountResource{}, nil
+}
